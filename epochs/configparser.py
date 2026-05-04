0da8456ee6073a3d527b6980c6c7a8d87c829525
Michael Galloy <mgalloy@gmail.com>
2026-05-03 23:03:57 -0600

    Better comments and error checking

--- epochs/configparser.py
+++ epochs/configparser.py
@@ -126,6 +126,8 @@ def _convert(value: str, type_value: type, is_list: bool = False) -> OptionValue
     if is_list:
         return [_convert(v, type_value, False) for v in _parse_list(value) if v != ""]
     else:
+        if type(value) == type_value:
+            return value
         if type_value == bool:
             if value.lower() in {"yes", "true", "1"}:
                 return True
@@ -337,6 +339,8 @@ class ConfigParser(configparser.ConfigParser):
         if self.specification is None:
             return True
 
+        # [TODO]: .has_option, .options, and .sections need to use inheritance
+
         # check that every option given by f is in specification
         if not allow_extra_options:
             for s in self.sections():
@@ -344,7 +348,7 @@ class ConfigParser(configparser.ConfigParser):
                     if self.has_option("DEFAULT", o):
                         continue
                     if not self.specification.has_option(s, o):
-                        print(f"section={s}, option={o}")
+                        print(f"section={s}, option={o} not in spec")
                         return False
 
         # check that all options without a default value are given by f
@@ -355,6 +359,7 @@ class ConfigParser(configparser.ConfigParser):
                 specline = self.specification.get(s, o)
                 spec = _parse_specline(specline)
                 if spec.required and not self.has_option(s, o):
+                    print(f"section={s}, option={o} not defined in spec or file")
                     return False
 
         # TODO: make sure values are the correct type?
